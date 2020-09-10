dc4bbe1a4529f0a0ba576e9e81682f255d5416c9
unknown <ngodongdac3005@gmail.com>
2020-09-10 18:17:45 +0700

    cors origin

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use('/test', (req,res)=>res.send("not found api"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.header("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', true);
 
   next(createError(404));
