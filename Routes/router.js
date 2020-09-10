e51a06ae0d9c2e570e14cec67ffa074db3f72bdb
chauquangtam <64776841+chauquangtam@users.noreply.github.com>
2020-09-10 17:09:28 +0700

    Update router.js
    thêm path router.use("/shop",require('./shopRouter'))

--- Routes/router.js
+++ Routes/router.js
@@ -4,7 +4,8 @@ const router = express.Router();
 // const usersRouter = require('./usersRouter');
 // const danhmucRouter = require('./danhmucRouter');
 
-router.use("/users",require('./usersRouter'))
+router.use("/users",require('./usersRouter'));
+router.use("/shop",require('./shopRouter'))
 // router.use("/danhmuc",danhmucRouter)
 
 module.exports = router;
