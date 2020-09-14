7d45de9ff805b6e28ae5b3ce802da16cf89a49a4
unknown <ngodongdac3005@gmail.com>
2020-09-14 13:03:36 +0700

    add procuct router

--- Routes/router.js
+++ Routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use("/users",require('./usersRouter'))
+router.use("/product",require('./productRouter'))
 router.use("/shop",require('./shopRouter'))
 router.use("/cart",require('./cartRouter'))
 router.use("/category",require('./categoryRouter'))
