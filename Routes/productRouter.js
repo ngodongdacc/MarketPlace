a676a663669d6a39154e02fd896a289088e78442
unknown <ngodongdac3005@gmail.com>
2020-09-15 12:04:29 +0700

    img

--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
@@ -42,17 +41,3 @@ router.post("/list/delete",productCtr.remove_list_product);
 router.get("/search",productCtr.search_product);
 
 module.exports = router
-=======
-const express = require('express');
-const router = express.Router();
-const productCtr = require("../Controllers/productController");
-
-router.post('/add', productCtr.postcreateProduct);
-router.post('/sua', productCtr.postcreateProduct);
-router.post('/xoa', productCtr.postcreateProduct);
-router.get('/lay', productCtr.postcreateProduct);
-
-
-
-module.exports = router;
->>>>>>> hung
