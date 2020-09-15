c67e9d37521751faf4c9a2c6a6023e0aaeeb9375
HungFpoly <phamcongquochung2000@gmail.com>
2020-09-15 08:28:34 +0700

    update brandOrigin

--- Controllers/brandOriginController.js
+++ Controllers/brandOriginController.js
@@ -5,20 +5,20 @@ const async = require("async");
 module.exports = {
     postcreateBrandOrigin: async (req, res, next) => {
         try {
-            const {country} = req.body
+            const { country } = req.body
 
             const newBrandOrigin = new BrandOrigin({
-                
+
                 country: req.body.country
             })
 
             BrandOriginService.createBrandOrigin(newBrandOrigin, (err, brandOrigin) => {
-                if (err) res.status(400).json({ message: "There was an error processing", errors: err, code: 0 });
+                if (err) res.status(400).json({ message: "There was an error processing", errors: err, code: 0 })
                 return res.send({
                     message: "create brandorigin success",
                     data: {
 
-                            country: brandOrigin.country
+                        country: brandOrigin.country
                     },
                     code: 1,
                     status: true
@@ -38,8 +38,9 @@ module.exports = {
 
         BrandOriginService.findOneBrandOriginByID(id, (err, resbrandOrigin) => {
             if (err) return res.status(400).json({ message: "There was an error processing", errors: err, status: false });
+            if (!resbrandOrigin.country) return res.status(400).json({ message: "not find country", errors: err, status: false });
 
-            BrandOrigin.findByIdAndRemove(id, {country:req.body.country}, (err, resRemoveBrandOrigin) => {
+            BrandOrigin.findByIdAndRemove(id, { country: req.body.country }, (err, resRemoveBrandOrigin) => {
                 if (err) return res.status(400).json({ message: "There was an error processing", errors: err, status: false });
                 res.json({
                     message: "Delete brandorigin success",
@@ -52,18 +53,20 @@ module.exports = {
     },
 
     postupdateBrandOrigin: async (req, res, next) => {
-        const brandOriginUpdate = { };
+        const brandOriginUpdate = {};
 
         if (req.body.country) brandOriginUpdate.country = req.body.country;
 
         const { id } = req.params
 
-        if (!id) return res.status(400).json({ message: "id is required", status: false, code: 0 })
+        if (!id) return res.status(400).json({ message: "id is required", status: false, code: 0 });
+        if (brandOriginUpdate.country) return res.status(400).json({ message: "country not null", status: false, code: 0 });
 
         BrandOriginService.findOneBrandOriginByID(id, (err, resFindBrandOrigin) => {
             if (err) return res.status(400).json({ message: "There was an error processing", errors: err, status: false });
+            if (!resFindBrandOrigin) return res.status(400).json({ message: "not find country", errors: err, status: false });
 
-            BrandOrigin.findByIdAndUpdate(id,{country:req.body.country},(err, resbrandOrigin) => {
+            BrandOrigin.findByIdAndUpdate(id, { country: req.body.country }, (err, resbrandOrigin) => {
                 if (err) return res.status(400).json({ message: "There was an error processing", errors: err, status: false });
                 res.json({
                     message: "update brandorigin success",
@@ -77,13 +80,13 @@ module.exports = {
 
     },
 
-    getProfile : async (req, res) => {
-       try{
-           const brandOrigin = await BrandOrigin.find()
-           res.json(brandOrigin)
-       }catch(err){
-           res.send('Error ' + err)
-       }
-      },
-    
+    getProfile: async (req, res) => {
+        try {
+            const brandOrigin = await BrandOrigin.find()
+            res.json(brandOrigin)
+        } catch (err) {
+            res.send('Error ' + err)
+        }
+    },
+
 }
