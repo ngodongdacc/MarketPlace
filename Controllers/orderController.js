36173644c99bb4de8bb5fb965968d01fb448b97f
Bui Hoang <buihoang@Buis-MacBook-Pro.local>
2020-09-15 07:52:23 +0700

    ssửa lại thêm sửa xoá của ORDER

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -1,118 +1,76 @@
 const Order = require("../Model/order");
-const orderService = require("../Services/orderService");
-const OrderService = require("../Services/orderService");
-
-
-const createOrder = async(req, res) => {
-    var newOrder = new Order({
-        UserId: req.body.UserId,
-        Product: req.body.Product,
-        Amount: res.body.Amount,
-        Price: res.body.Price,
-        Name: req.body.Name,
-        Address: req.body.Address,
-        Payment: req.body.Payment,
-        Status: req.body.Status,
-        IntoMoney:req.body.IntoMoney,
-        IdCart: req.body.IdCart
-    })
-    Order.create(newOrder,function(err, resData){
-        if(err) res.status(400).json({message: "Có lỗi trong quá trình xử lý",errors: err, status: false});
-        res.json({
-            message: "thêm đơn hàng success !",
-            data: resData,
-            status: true
-        })
-    })
-}
-
-const deleteOrder = async(req, res) => {
-    var deleteOrder = {
-        UserId: req.body.UserId,
-        Product: req.body.Product,
-        Amount: res.body.Amount,
-        Price: res.body.Price,
-        Name: req.body.Name,
-        Address: req.body.Address,
-        Payment: req.body.Payment,
-        Status: req.body.Status,
-        IntoMoney:req.body.IntoMoney,
-        IdCart: req.body.IdCart
-    }
-    orderService.deleteOrder(deleteOrder, function(err, resData){
-        if(err){
-            return res.send({
-                message: "delete failse",
-                errors: err,
-                status: false,
-            }).status(400)
-        }
-        res.send({
-            message: "delete success!",
-            data: resData,
-            status: true
-        })
-    })
-}
-
-const updateOrder = async(req, res) => {
-    var updateOrder = {
-        UserId: req.body.UserId,
-        Product: req.body.Product,
-        Amount: res.body.Amount,
-        Price: res.body.Price,
-        Name: req.body.Name,
-        Address: req.body.Address,
-        Payment: req.body.Payment,
-        Status: req.body.Status,
-        IntoMoney:req.body.IntoMoney,
-        IdCart: req.body.IdCart
-    }
-    OrderService.updateOrder(updateOrder, function(err, resData){
-        if(err){
-            return res.send({
-                message: "Update error",
-                errors: err,
-                status: false,
-            }).status(400)
-        }
-        res.send({
-            message: "update success!",
-            data: resData,
-            status: true
-        })
-    })
-}
-const getOrder = async(req, res) => {
-    var getOrder = {
-        UserId: req.body.UserId,
-        Product: req.body.Product,
-        Amount: res.body.Amount,
-        Price: res.body.Price,
-        Name: req.body.Name,
-        Address: req.body.Address,
-        Payment: req.body.Payment,
-        Status: req.body.Status,
-        IntoMoney:req.body.IntoMoney,
-        IdCart: req.body.IdCart
-    };
-
-    OrderService.getOrderById(getSubCate, function(err, resData){            
-        if(err){
-            return res.send({
-                message: "get Category failse",
-                errors: err,
-                status: false,
-            }).status(400)
-        }
-        res.send({
-            message: "get succsess",
-            data: resData,
-            status: true
-        })
-    })
-}
+const async = require("async");
+const Users = require("../Model/users");
+const userServic = require("../Services/usersService");
 
 module.exports = {
-    createOrder, deleteOrder, updateOrder,getOrder,
+    createOrder: (req, res)=>{
+        const order = req.body
+        if(!order.UserId) return res.status(400).json({message: "Vui lòng nhập UserID", status:false});
+        if(!order.IdCart) return res.status(400).json({message: "Vui lòng nhập IdCart", status:false});
+        if(!order.Name) return res.status(400).json({message: "Vui lòng nhập Name", status: false});
+
+        Order.create(order, (err,resData) => {
+            if(err) return res.status(400).json({message: "Có lỗi trong quá trình xử lý",errors: err,status:false});
+            res.json({
+                message: "Tạo đơn hàng thành công !",
+                data: resData,
+                status: true
+            })
+        })
+    },
+    updateOrder: (req,res) => {
+        const order = req.body
+        const id = req.params.id
+        if(!id) return res.status(400).json({message: "Vui lòng nhập Id", status:false})
+        Order.findById(id,(err, resOrder) => {
+            if(err) return res.status(400).json({message: "Có lỗi trong quá trình xử lý",errors: err,status:false});
+            if(!resOrder) return res.json({message: "Không tìm thấy id đơn hàng",data: null,status:false});
+
+        Order.findByIdAndUpdate(resOrder._id, {$set: order},options,(err, resUpdate) => {
+                if(err) return res.status(400).json({message: "Có lỗi trong quá trình xử lý",errors: err,status:false});
+                res.json({
+                    message: "Cập nhật order thành công",
+                    data: resUpdate,
+                    status: true
+                })
+             })
+        })
+    },
+    getOrder: async(req, res) => {
+        const id = req.body.id
+        if(!id) return res.status(400).json({message: "Vui lòng nhập Id", status:false});
+
+
+        Order.findById(id, (err,resOrder) => {
+            if(err) return res.status(400).json({message: "Có lỗi trong quá trình xử lứ, vui lòng thử lại", er: err, status: false});
+            if(!resOrder) return res.status(400).json({message: "Không tim thấy đơn hàng !", data: null, status: false});
+
+            res.json({
+                message: "Lấy chi tiết đơn hàng thành công",
+                data: resOrder,
+                status: true
+            })
+        })
+    },
+    deleteOrder: async(req,res) => {
+        const id = req.params.id
+        if(!id) return res.status(400).json({message: "Vui lòng nhập Id", status:false});
+
+            Order.findById(id,(err, resOrder)=>{
+                if(err) return res.stutus(400).json({message: "OOp lỗi rồi", err:err, status:false});
+                if(!resOrder) return res.stutus(400).json({message: "không tìm đc ID này, vui lòng thử lại", data: null, status:false});
+
+            Order.findByIdAndRemove(resOrder._id,function(err,resData){
+                if(err) return res.status(400).json({message: "Có lỗi trong quá trình xử lý",errors: err,status:false});
+                res.json({
+                    message: "Xóa sản phẩm thành công",
+                    data: resData,
+                    status: true
+                })
+            })
+        })
+    }
 }
+
+
