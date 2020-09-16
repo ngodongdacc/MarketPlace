6cfd26f25d472a1e04ecfa71d563cb43d01e5ea0
chauquangtam <chauquangtam2000@gmail.com>
2020-09-17 00:35:42 +0700

    update api delete quantity for product

--- Controllers/CartController.js
+++ Controllers/CartController.js
@@ -12,7 +12,7 @@ module.exports = {
             Product.findOne({ _id: ProductId }, async (err, resFindProduct) => {
                 if (err) return res.status(400).json({ message: "Sản phẩm không còn tồn tại", errors: err, status: false });
                 if (resFindProduct) {
-                    resFindProduct.Quantity = 2;
+                    resFindProduct.Quantity = Quantity;
                     const Total = Quantity * resFindProduct.Price;
                     Cart.findOne({ UserId: UserId }, async (err, resFindUser) => {
                         if (err) return res.status(400).json({ message: "Không tìm thấy user", errors: err, status: false });
@@ -21,36 +21,44 @@ module.exports = {
                             if (itemIndex > -1) {
                                 //product exists in the cart, update the quantity
                                 resFindUser.ListProduct[itemIndex].Quantity += Quantity;
-                              let totals =await resFindUser.ListProduct.reduce((acc, next) => 
+                                let totals = await resFindUser.ListProduct.reduce((acc, next) =>
                                     acc + next.Quantity
-                                ,0);
-                                let prices =await resFindUser.ListProduct.reduce((acc, next) => 
-                                acc + (next.Price*next.Quantity) 
-                            ,0);
-                                resFindUser.SubTotal=await totals;
-                                resFindUser.SubPrice=await prices;
-                                console.log("res",resFindUser)
-                                let CartUpdate={};
-                                CartUpdate=await resFindUser;
-                                Cart.findOneAndUpdate({_id:resFindUser._id}, {ListProduct: CartUpdate.ListProduct,
-                                    SubTotal:CartUpdate.SubTotal,
-                                    SubPrice:CartUpdate.SubPrice
-                                
+                                    , 0);
+                                let prices = await resFindUser.ListProduct.reduce((acc, next) =>
+                                    acc + (next.Price * next.Quantity)
+                                    , 0);
+                                resFindUser.SubTotal = await totals;
+                                resFindUser.SubPrice = await prices;
+                                let CartUpdate = {};
+                                CartUpdate = await resFindUser;
+                                Cart.findOneAndUpdate({ _id: resFindUser._id }, {
+                                    ListProduct: CartUpdate.ListProduct,
+                                    SubTotal: CartUpdate.SubTotal,
+                                    SubPrice: CartUpdate.SubPrice
+
                                 }, function (err, resData) {
                                     if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
-                                  console.log(resData)
                                     res.json({
                                         message: "Cập nhật mới sản phẩm vào giỏ hàng thành công",
                                         status: true
                                     })
                                 });
                             } else {
-                                resFindProduct.Quantity = Quantity;
-                                resFindProduct.Total = Total;
+                                let totals = await resFindUser.ListProduct.reduce((acc, next) =>
+                                    acc +  next.Quantity
+                                    , 0);
+                                let prices = await resFindUser.ListProduct.reduce((acc, next) =>
+                                    acc + (next.Price * next.Quantity)
+                                    , 0);
+                                    resFindUser.SubTotal = await totals;
+                                    resFindUser.SubPrice = await prices;
+                                    console.log(resFindUser.SubTotal)
+                                    console.log(resFindUser.SubPrice)
                                 //product does not exists in cart, add new item
                                 resFindUser.ListProduct.push(
                                     resFindProduct
                                 );
+                               
                                 resFindUser.SubTotal = resFindUser.ListProduct.map(ListProduct => ListProduct.Total).reduce((acc, next) => acc + next);
                                 Cart.findByIdAndUpdate(resFindUser._id, resFindUser, function (err, resData) {
                                     if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
@@ -58,28 +66,35 @@ module.exports = {
                                         message: "Cập nhật mới sản phẩm vào giỏ hàng thành công",
                                         status: true
                                     })
-
                                 });
                             }
                         } else {
                             //no cart for user, create new cart
-                            const SubTotal = Total;
-                            const    SubPrice =Quantity *resFindProduct.Price;
-                            const ListProduct=[];
-                         ListProduct.push(
+
+                            let totals = await resFindUser.ListProduct.reduce((acc, next) =>
+                                acc + next.Quantity
+                                , 0);
+                            let prices = await resFindUser.ListProduct.reduce((acc, next) =>
+                                acc + (next.Price * next.Quantity)
+                                , 0);
+                            resFindUser.SubTotal = await totals;
+                            resFindUser.SubPrice = await prices;
+
+
+                            const ListProduct = [];
+                            ListProduct.push(
                                 resFindProduct
                             );
-                         console.log("quantity: ",Quantity)
                             Cart.create({
                                 UserId,
                                 ListProduct,
                                 Des,
                                 Title,
-                                SubTotal,
-                                SubPrice
+                                SubTotal: resFindUser.SubTotal,
+                                SubPrice: resFindUser.SubPrice
                             }, function (err, resBRC) {
                                 if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
-                           
+
                                 res.json({
                                     message: "Thêm mới sản phẩm thành công",
                                     data: resBRC,
@@ -101,13 +116,20 @@ module.exports = {
     },
     deleteCart: async (req, res) => {
         const { ProductId, UserId } = req.body
-        console.log(UserId)
+        // console.log(UserId)
         Cart.findOne({ UserId: UserId }, async (err, resFindUser) => {
             if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
             if (!resFindUser) return res.status(400).json({ message: "Không tìm thấy User", data: null, status: false });
             if (resFindUser) {
                 resFindUser.ListProduct.findIndex(p => p._id == ProductId) !== -1 && resFindUser.ListProduct.splice(resFindUser.ListProduct.findIndex(p => p._id == ProductId), 1)
-
+                let totals = await resFindUser.ListProduct.reduce((acc, next) =>
+                    acc + next.Quantity
+                    , 0);
+                let prices = await resFindUser.ListProduct.reduce((acc, next) =>
+                    acc + (next.Price * next.Quantity)
+                    , 0);
+                resFindUser.SubTotal = await totals;
+                resFindUser.SubPrice = await prices;
                 Cart.findByIdAndUpdate(resFindUser._id, resFindUser, (err, resRemove) => {
                     if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
                     res.json({
@@ -119,16 +141,56 @@ module.exports = {
             }
         })
     },
+    delete_Quantity_OfCart: async (req, res) => {
+        let { id, UserId, Quantity } = req.body
+        Cart.findOne({ UserId: UserId }, async (err, resUserCart) => {
+            if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
+            if (!resUserCart) return res.status(400).json({ message: "Không tìm thấy User", data: null, status: false });
+            if (resUserCart) {
+                let itemIndex = await resUserCart.ListProduct.findIndex(p => p._id == id);
+                if (itemIndex > -1) {
+                    if (resUserCart.ListProduct[itemIndex].Quantity > Quantity) {
+                        resUserCart.ListProduct[itemIndex].Quantity -= Quantity;
+                        let totals = await resUserCart.ListProduct.reduce((acc, next) =>
+                            acc + next.Quantity
+                            , 0);
+                        let prices = await resUserCart.ListProduct.reduce((acc, next) =>
+                            acc + (next.Price * next.Quantity)
+                            , 0);
+                        resUserCart.SubTotal = await totals;
+                        resUserCart.SubPrice = await prices;
+                        Cart.findByIdAndUpdate(resUserCart._id, resUserCart, (err, resRemove) => {
+                            if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
+                            res.json({
+                                message: "Xóa sản phẩm thành công",
+                                data: resRemove,
+                                status: true
+                            })
+                        })
+                    } else {
+                        res.json({
+                            message: "Không thể xóa giá trị này",
+                            data: null,
+                            status: false
+                        })
+                    }
+
+                } else {
+                    res.json({
+                        message: "Có lỗi xảy ra",
+                        // data: resRemove,
+                        status: false
+                    })
+                }
+            }
+        })
+    },
     delete_All_ForUser: async (req, res) => {
         const id = req.params.id
-        console.log(id)
         Cart.findOne({ _id: id }, async (err, resFindUser) => {
-            console.log(resFindUser)
             if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
             if (!resFindUser) return res.status(400).json({ message: "Không tìm thấy User", data: null, status: false });
             if (resFindUser) {
-                // resFindUser.ListProduct = resFindUser.ListProduct.remove(resFindUser.ListProduct)
-                // resFindUser.ListProduct.findIndex(p => p.ProductId == ProductId) !== -1 && resFindUser.ListProduct.splice(resFindUser.ListProduct.findIndex(p => p.ProductId == ProductId), 1)
                 Cart.deleteOne({ _id: resFindUser._id }, (err, resRemove) => {
                     if (err) return res.status(400).json({ message: "Có lỗi trong quá trình xử lý", errors: err, status: false });
                     res.json({
--- Routes/cartRouter.js
+++ Routes/cartRouter.js
@@ -6,8 +6,10 @@ var cartCtr = require("../Controllers/CartController");
 router.post('/add', cartCtr.postCart);
 router.post('/update', cartCtr.postCart);
 router.post('/delete', cartCtr.deleteCart);
+router.post('/delete-quantity', cartCtr.delete_Quantity_OfCart);
 router.get('/deleteAllProduct/:id', cartCtr.delete_All_ForUser);
 router.get('/get', cartCtr.getCart);
 router.get('/:id', cartCtr.showCartForUser);
+
 // router.post('/search', cartCtr.searchCart);
 module.exports = router
