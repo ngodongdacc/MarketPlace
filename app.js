600716e44b38fc8e6c2532dc1db81f0865559998
unknown <ngodongdac3005@gmail.com>
2020-09-17 09:13:46 +0700

    origin method

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.use(passport.session());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
