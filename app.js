3c755ce225c5afe617527c1f3f684a94500d4740
unknown <ngodongdac3005@gmail.com>
2020-09-17 10:11:13 +0700

    cors local host

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", ["https://localhost:3000","*"]);
+  res.header("Access-Control-Allow-Origin", ["http://localhost:3000","*"]);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
