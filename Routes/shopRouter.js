2bce7305f5d95b2db6d462deccfdb02eb779d0b0
ngodongdacc <ngodongdac3005@gmail.com>
2020-09-28 11:50:20 +0700

    shop

--- Routes/shopRouter.js
+++ Routes/shopRouter.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {checkSignIn} = require("../middleware/auth")
+const {checkSignIn,checkLogInShop} = require("../middleware/auth")
 var shopCtr = require("../Controllers/ShopController");
 
 router.post('/add', shopCtr.postshop); // thêm mới một cửa hàng
 router.post('/login', shopCtr.post_login); // Đăng nhập
-router.get('/list', checkSignIn(),shopCtr.getShop); // lấy hết danh sách cửa hàng
+router.get('/list', checkLogInShop(),shopCtr.getShop); // lấy hết danh sách cửa hàng
 router.post('/search', shopCtr.searchShop); // chưa đc
 router.post('/delete/list-shop', shopCtr.delete_listShop); // xóa danh sách cửa hàng đã chọn
 router.get('/shop-details', shopCtr.shop_details_forIdOwnerShop); // hiện thị thông tin chi tiết 1 cửa hàng
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 
 module.exports = {
-    checkSignIn: () => passport.authenticate('jwt', { session: false })
+    checkSignIn: () => passport.authenticate('jwt', { session: false }),
+    checkLogInShop: () => passport.authenticate('shop-jwt', { session: false })
 }
