5c5fb5d644d1e4653592e0e7117308b7c4101843
Bui Hoang <buihoang@Buis-MacBook-Pro.local>
2020-09-28 11:48:22 +0700

    huy don hang 80%

--- Routes/orderRouter.js
+++ Routes/orderRouter.js
@@ -14,6 +14,7 @@ router.get("/getOrderByUsers",orderController.getOrderByIdUsers);
 router.get("/getOrderByCart",orderController.getOrderByCart);
 router.get("/serchListOrderByShop",orderController.searchListOrderByShop);
 router.post("/list/delete",orderController.deleteListOrder);
+router.post("/updateStutus/:id",orderController.updateStatusOrder);
 
 
 
